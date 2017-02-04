a68448f3755bf8d7d89bc8fea43448fa1232c35a
Cdkushni <colinkushniruk@gmail.com>
2017-02-03 20:41:16 -0700

    Starting the SizeBook with the expandable list view UI

---
+++ app/src/main/java/com/cmput301project/colin/sizebook/ExpListAdapter.java
@@ -0,0 +1,101 @@
+package com.cmput301project.colin.sizebook;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by Colin on 2/3/2017.
+ */
+
+public class ExpListAdapter extends BaseExpandableListAdapter {
+    private Context context;
+    private List<String> listDataHeader;
+    private HashMap<String, List<String>> listHashMap;
+
+    public ExpListAdapter(Context context, List<String> listDataHeader, HashMap<String, List<String>> listHashMap) {
+        this.context = context;
+        this.listDataHeader = listDataHeader;
+        this.listHashMap = listHashMap;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return listDataHeader.size();
+    }
+
+    @Override
+    public int getChildrenCount(int i) {
+        return listHashMap.get(listDataHeader.get(i)).size();
+    }
+
+    @Override
+    public Object getGroup(int i) {
+        return listDataHeader.get(i);
+    }
+
+    @Override
+    public Object getChild(int i, int i1) {
+        // i = group position, i1 = item position
+        return listHashMap.get(listDataHeader.get(i)).get(i1);
+    }
+
+    @Override
+    public long getGroupId(int i) {
+        return i;
+    }
+
+    @Override
+    public long getChildId(int i, int i1) {
+        return i1;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int i, boolean b, View view, ViewGroup viewGroup) {
+        String hdrTitle = (String)getGroup(i);
+        if(view == null){
+            LayoutInflater inflater = (LayoutInflater)this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.list_group,null);
+        }
+        TextView lblListHeader = (TextView)view.findViewById(R.id.lblListHeader);
+        lblListHeader.setTypeface(null, Typeface.BOLD);
+
+        String hdrTitleStr = "SizeBook";
+        char[] headerTitle = hdrTitleStr.toCharArray();
+
+        lblListHeader.setText("SizeBook");
+        return view;
+    }
+
+    @Override
+    public View getChildView(int i, int i1, boolean b, View view, ViewGroup viewGroup) {
+        final String childText = (String)getChild(i,i1);
+        if(view == null){
+            LayoutInflater inflater = (LayoutInflater)this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.list_item,null);
+        }
+
+        TextView txtListChild = (TextView)view.findViewById(R.id.lblListItem);
+        txtListChild.setText(childText);
+        return view;
+    }
+
+    @Override
+    public boolean isChildSelectable(int i, int i1) {
+        return true;
+    }
+}
